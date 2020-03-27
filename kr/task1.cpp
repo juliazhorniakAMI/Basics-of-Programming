3d8067f752fb6a47da7396b2bd04f6d819a5f43e
juliazhorniakAMI <62382551+juliazhorniakAMI@users.noreply.github.com>
2020-03-27 11:07:44 +0300

    Update task1.cpp

--- kr/task1.cpp
+++ kr/task1.cpp
@@ -1,1 +1,47 @@
 
+
+
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include <string>
+
+
+
+
+long factorial(int n) {
+    long f = 1;
+    for (int i = 2;i <= n;i++) {
+        f *= i;
+    
+    }
+    return f;
+}
+
+
+
+
+
+
+int main(){
+
+    int n;
+    cout << "Enter number: " << endl;
+    cin >> n;
+    cout << "n!= " << factorial(n);
+
+
+   
+
+
+
+   
+
+
+    
+
+   
+
+   
+    return 0;
+}
